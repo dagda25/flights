77686bfb347e6e2592a99ed335b5e84c27f83353
dagda25 <dagda25@gmail.com>
2021-03-19 18:12:16 +0300

    package

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require(`express`);
-var cors = require('cors')
+const cors = require(`cors`);
 const data = require(`./data.json`);
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.get(`/api`, (req, res) => {
