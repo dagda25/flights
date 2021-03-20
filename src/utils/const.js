64fce8e23bc5236f91108912ddc8aa51e1153c52
dagda25 <dagda25@gmail.com>
2021-03-20 09:49:15 +0300

    delete files

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,16 +1,5 @@
 
 export const AppRoute = {
   ROOT: `/`,
-  ITEM: `item`
 };
-
-export const APIRoute = {
-  LIST: `/pokemon?limit=100`,
-  ROOT: `/pokemon/`,
-};
-
-export const settings = {
-  FETCH_COUNT: 30,
-};
-
 
